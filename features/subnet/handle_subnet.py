5c4856bb67343d7a97777fa2bfb86ec6f46e55ef
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-25 23:58:45 -0600

    Create handle_subnet.py

---
+++ features/subnet/handle_subnet.py
@@ -0,0 +1,8 @@
+def handle_subnet():
+    network = ipaddress.ip_network(f"{ip}/{mask}", strict=False)
+    net_addr = str(network.network_address)
+    broadcast_addr = str(network.broadcast_address)
+    usable_range = f"{str(network[1])} - {str(network[-2])}"
+    host_count = network.num_addresses
+    response = f"**Here are the details for subnet {network}**: \n\n**Network address**: {net_addr}\n**Broadcast address**: {broadcast_addr}\n**Usable IP range**: {usable_range}\n**Number of hosts**: {host_count}"
+    return response
