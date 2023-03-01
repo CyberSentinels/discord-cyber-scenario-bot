a4b592b29a2a4a7bec7471279e2a1433c614f582
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-28 20:42:58 -0600

    Update handle_cissp.py

--- features/cissp/handle_cissp.py
+++ features/cissp/handle_cissp.py
@@ -9,14 +9,15 @@ def handle_cissp():
     question = random.choice(cisspdict)
     prompt = question["question"]
     answers = question["answers"]
+    correctanswer = question["correctanswer"]
+    reasoning = question["reasoning"] or None
     options = []
     for key, value in answers.items():
         if key != "correctanswer":
             options.append(f"**{key.upper()}**: {value}")
     options = "\n".join(options)
     correct_answer = answers["correctanswer"]
-    if "reasoning" in question:
-        reasoning = question["reasoning"]
+    if "reasoning" is not None:
         response = f"**Here's a CISSP question for you**:\n\n**Question**: {prompt}\n\n**Options**: \n{options}\n\n**Correct Answer**: ||{correct_answer}||\n\n**Reasoning**: ||{reasoning}||"
     else:
         response = f"**Here's a CISSP question for you**:\n\n**Question**: {prompt}\n\n**Options**: \n{options}\n\n**Correct Answer**: ||{correct_answer}||"
