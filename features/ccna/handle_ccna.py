b9323b06f75bf27b913f5c74bf2ce3cb9c2753af
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-02-28 19:42:59 -0600

    Update handle_ccna.py

--- features/ccna/handle_ccna.py
+++ features/ccna/handle_ccna.py
@@ -6,7 +6,7 @@ from .ccnadict import ccnadict
 import random
 
 def handle_ccna():
-    question = random.choice(ccna)
+    question = random.choice(ccnadict)
     prompt = question["question"]
     answers = question["answers"]
     options = []
