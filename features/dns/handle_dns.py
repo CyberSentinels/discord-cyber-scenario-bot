b0491d812fe10fb2b0754458b059fd28b9e17628
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-03-03 14:42:01 -0600

    Clean Up DNS Command Output

--- features/dns/handle_dns.py
+++ features/dns/handle_dns.py
@@ -2,48 +2,71 @@ import discord
 import dns.resolver
 
 async def handle_dns(domain):
+    resolver = dns.resolver.Resolver()
+    resolver.lport = 0
     query = domain
-    embed = discord.Embed(title=f'DNS Records for {query}')
+    
+    a_records = []
+    aaaa_records = []
+    txt_records = []
+    ns_records = []
+    cname_records = []
+    dmarc_records = []
+
     try:
         response = dns.resolver.query(query, 'A')
         for answer in response:
-            embed.add_field(name='A Record', value=answer.address)
+            a_records.append(answer.address)
     except dns.resolver.NXDOMAIN:
-        embed.add_field(name='Error', value=f'Unable to resolve {query}')
+        a_records.append("No A records found")
 
     try:
         response = dns.resolver.query(query, 'AAAA')
         for answer in response:
-            embed.add_field(name='AAAA Record', value=answer.address)
+            aaaa_records.append(answer.address)
     except dns.resolver.NoAnswer:
-        pass
+        aaaa_records.append("No AAAA records found")
 
     try:
         response = dns.resolver.query(query, 'TXT')
         for answer in response:
-            embed.add_field(name='TXT Record', value=answer.to_text())
+            txt_records.append(answer.to_text())
     except dns.resolver.NoAnswer:
-        pass
+        txt_records.append("No TXT records found")
 
     try:
         response = dns.resolver.query(query, 'NS')
         for answer in response:
-            embed.add_field(name='NS Record', value=answer.to_text())
+            ns_records.append(answer.to_text())
     except dns.resolver.NoAnswer:
-        pass
+        ns_records.append("No NS records found")
 
     try:
         response = dns.resolver.query(query, 'CNAME')
         for answer in response:
-            embed.add_field(name='CNAME Record', value=answer.to_text())
+            cname_records.append(answer.to_text())
     except dns.resolver.NoAnswer:
-        pass
+        cname_records.append("No CNAME records found")
 
     try:
         response = dns.resolver.query('_dmarc.' + query, 'TXT')
         for answer in response:
-            embed.add_field(name='DMARC Record', value=answer.to_text())
+            dmarc_records.append(answer.to_text())
     except dns.resolver.NoAnswer:
-        pass
+        dmarc_records.append("No DMARC records found")
+
+    embed = discord.Embed(title=f"DNS Lookup - Results for {query}")
+    if a_records:
+        embed.add_field(name="A Records", value="\n".join(a_records), inline=False)
+    if aaaa_records:
+        embed.add_field(name="AAAA Records", value="\n".join(aaaa_records), inline=False)
+    if ns_records:
+        embed.add_field(name="NS Records", value="\n".join(ns_records), inline=False)
+    if txt_records:
+        embed.add_field(name="TXT Records", value="\n".join(txt_records), inline=False)
+    if cname_records:
+        embed.add_field(name="CNAME Records", value="\n".join(cname_records), inline=False)
+    if dmarc_records:
+        embed.add_field(name="DMARC Records", value="\n".join(dmarc_records), inline=False)
 
     return embed
