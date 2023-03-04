e4546ed8ca54b83783a0983a0a9d639a55a8fe53
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-03-03 19:40:45 -0600

    Update handle_dns.py

--- features/dns/handle_dns.py
+++ features/dns/handle_dns.py
@@ -17,42 +17,42 @@ async def handle_dns(domain):
         response = dns.resolver.query(query, 'A')
         for answer in response:
             a_records.append(answer.address)
-    except dns.resolver.NXDOMAIN:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         a_records.append("No A records found")
 
     try:
         response = dns.resolver.query(query, 'AAAA')
         for answer in response:
             aaaa_records.append(answer.address)
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         aaaa_records.append("No AAAA records found")
 
     try:
         response = dns.resolver.query(query, 'TXT')
         for answer in response:
             txt_records.append(answer.to_text())
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         txt_records.append("No TXT records found")
 
     try:
         response = dns.resolver.query(query, 'NS')
         for answer in response:
             ns_records.append(answer.to_text())
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         ns_records.append("No NS records found")
 
     try:
         response = dns.resolver.query(query, 'CNAME')
         for answer in response:
             cname_records.append(answer.to_text())
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         cname_records.append("No CNAME records found")
 
     try:
         response = dns.resolver.query('_dmarc.' + query, 'TXT')
         for answer in response:
             dmarc_records.append(answer.to_text())
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
         dmarc_records.append("No DMARC records found")
 
     embed = discord.Embed(title=f"DNS Lookup - Results for {query}")
