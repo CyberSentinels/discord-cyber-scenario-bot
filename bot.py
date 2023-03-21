3f708873f50edc34e3bcadda56f67e90e0118a02
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-03-21 18:22:15 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,10 +106,10 @@ async def on_reaction_add(reaction, user):
         question = question_dict[question_number]
         correct_answer = question["correctanswer"]
         if answer == correct_answer:
-            await reaction.message.reply(f"{user.mention} 🎉 Congratulations, your answer is correct!", mention_author=False)
+            await user.send(f"🎉 Congratulations, your answer '{answer}' is correct!")
         else:
+            #await user.send(f"🤔 Your answer '{answer}' is incorrect. The correct answer is '{correct_answer}'.")
             return
-            #await reaction.message.reply(f"{user.mention} 🤔 Your answer '{answer}' is incorrect. The correct answer is '{correct_answer}'.", mention_author=False, ephemeral=True)
 
 
 @client.hybrid_command(
