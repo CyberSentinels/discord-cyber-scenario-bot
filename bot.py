58145368843625b3211a2cd2145c3137ab9aadfa
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-03-26 03:21:26 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -502,7 +502,6 @@ async def whois(ctx, domain: str):
 # Define the leaderboard update task
 @tasks.loop(hours=1, minutes=0)
 async def update_leaderboard_task():
-    time.sleep(360)
     await update_leaderboard()
 
 # Define the random quiz task to run at 12:00pm every day
