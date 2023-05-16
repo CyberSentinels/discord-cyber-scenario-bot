2287d924e7e436e6fef4076607045d3de632025e
1nf053c <1nf053c>
2023-05-16 09:43:58 -0500

    persist leaderboard encoding in different channel

--- features/update_leaderboard/create_leaderboard_embed.py
+++ features/update_leaderboard/create_leaderboard_embed.py
@@ -9,14 +9,14 @@ from features.update_leaderboard.sort_overall_user_scores import sort_overall_us
 
 
 async def create_leaderboard_embed(user_scores, question_dict_mapping, member_dict):
-    leaderboard_embed = Embed(
+    leaderboard= Embed(
         title="Quiz Commands Leaderboard", color=0x006400)
 
     # leaderboard embed: overall
     sorted_overall_user_scores = sort_overall_user_scores(user_scores)
     overall_leaderboard_str = create_overall_leaderboard_str(
         sorted_overall_user_scores, member_dict)
-    leaderboard_embed.add_field(
+    leaderboard.add_field(
         name="Overall", value=overall_leaderboard_str, inline=False)
 
     # leaderboard embed: each quiz leaderboard
@@ -27,17 +27,21 @@ async def create_leaderboard_embed(user_scores, question_dict_mapping, member_di
             x[1]["correct"], x[1]["incorrect"]), reverse=True)
         prefix_leaderboard_desc = create_quiz_leaderboard_str(
             sorted_users_by_quiz, member_dict)
-        leaderboard_embed.add_field(
+        leaderboard.add_field(
             name=prefix.upper(), value=prefix_leaderboard_desc, inline=False)
 
-    # leaderboard embed: base64 encoded user_scores
-    user_scores_json = json.dumps(user_scores)
-    user_scores_base64 = base64.b64encode(user_scores_json.encode()).decode()
-    leaderboard_embed.add_field(
-        name="Parity", value=f"```{user_scores_base64}```", inline=False)
-
     # leaderboard embed: footer
-    leaderboard_embed.set_footer(
+    leaderboard.set_footer(
         text="Note: The leaderboard is updated once per hour. \n To learn more about the quiz commands, run `/commands` in #bot-commands")
 
-    return leaderboard_embed
+    return leaderboard
+
+async def create_leaderboard_persistance_embed(user_scores):
+    # leaderboard embed: base64 encoded user_scores
+    leaderboard_persistence = Embed(
+        title="Leaderboard Persistance", color=0x006400)
+    user_scores_json = json.dumps(user_scores)
+    user_scores_base64 = base64.b64encode(user_scores_json.encode()).decode()
+    leaderboard_persistence.add_field(
+        name="Parity", value=f"```{user_scores_base64}```", inline=False)
+    return leaderboard_persistence
