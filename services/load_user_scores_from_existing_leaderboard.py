01424ed271e1a13d892d2ec0c62fa116cbe508af
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-21 00:14:18 -0500

    more error handling

--- services/load_user_scores_from_existing_leaderboard.py
+++ services/load_user_scores_from_existing_leaderboard.py
@@ -5,13 +5,22 @@ from services.try_decode_base64_json import try_decode_base64_json
 
 async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
     leaderboard_messages = []
-    async for message in leaderboard_channel.history():
-        if message.author == client.user:
-            leaderboard_messages.append(message)
+    try:
+        async for message in leaderboard_channel.history():
+            if message.author == client.user:
+                leaderboard_messages.append(message)
+    except Exception as e:
+        raise ValueError(f"Error occurred while fetching leaderboard messages: {str(e)}")
     if not leaderboard_messages:
         raise ValueError("ERROR: no leaderboard messages found in leaderboard channel")
-    user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_messages)
-    return try_decode_base64_json(user_scores_base64)
+    try:
+        user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_messages)
+    except Exception as e:
+        raise ValueError(f"Error occurred while processing leaderboard messages: {str(e)}")
+    try:
+        return try_decode_base64_json(user_scores_base64)
+    except Exception as e:
+        raise ValueError(f"Error occurred while decoding user scores: {str(e)}")
 
 async def test_load_user_scores_from_existing_leaderboard():
     from services.__fixtures__.mocks import (
