25861e5589e5c0276ece93eeb9a857a01eb7605b
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-21 00:12:09 -0500

    loop through all messages

--- services/load_user_scores_from_existing_leaderboard.py
+++ services/load_user_scores_from_existing_leaderboard.py
@@ -3,18 +3,15 @@ from services.get_encoded_user_scores_from_msg_embeds import (
 )
 from services.try_decode_base64_json import try_decode_base64_json
 
-
 async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
-    leaderboard_message = None
+    leaderboard_messages = []
     async for message in leaderboard_channel.history():
         if message.author == client.user:
-            leaderboard_message = message
-            break
-    if leaderboard_message is None:
-        raise ValueError("ERROR: no leaderboard message found in leaderboard channel")
-    user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_message)
+            leaderboard_messages.append(message)
+    if not leaderboard_messages:
+        raise ValueError("ERROR: no leaderboard messages found in leaderboard channel")
+    user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_messages)
     return try_decode_base64_json(user_scores_base64)
-
 
 async def test_load_user_scores_from_existing_leaderboard():
     from services.__fixtures__.mocks import (
