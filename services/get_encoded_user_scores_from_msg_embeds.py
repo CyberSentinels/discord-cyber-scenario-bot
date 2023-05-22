19c9c0b79fd6a0a8b22fe783f873230d319391a7
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-21 22:51:45 -0500

    Revert "Merge branch 'leaderboard-test'"
    This reverts commit a349d07dfb88dfc17c94bebc839854620bf5e062, reversing
    changes made to 446cb298da2bb293aacd4444ecf854b1c4e6bc4f.

--- services/get_encoded_user_scores_from_msg_embeds.py
+++ services/get_encoded_user_scores_from_msg_embeds.py
@@ -1,13 +1,10 @@
-def get_encoded_user_scores_from_msg_embeds(leaderboard_messages):
+def get_encoded_user_scores_from_msg_embeds(leaderboard_message):
     chunks = []
-    try:
-        for message in leaderboard_messages:
-            for embed in message.embeds:
-                for field in embed.fields:
-                    if field.name.startswith("chunk:"):
-                        chunks.append(field.value.strip("```"))
-    except Exception as e:
-        raise ValueError(f"Error occurred while processing leaderboard messages: {str(e)}")
+    if leaderboard_message is not None:
+        for embed in leaderboard_message.embeds:
+            for field in embed.fields:
+                if field.name[0:6] == "chunk:":
+                    chunks.append(field.value.strip("```"))
     chunks_str = "".join(chunks)
     return chunks_str
 
