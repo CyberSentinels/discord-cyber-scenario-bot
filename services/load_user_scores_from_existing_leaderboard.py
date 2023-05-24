a1a66c1bbf21496fe002370e958d243533ac631a
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 18:27:42 -0500

    clearnup

--- services/load_user_scores_from_existing_leaderboard.py
+++ services/load_user_scores_from_existing_leaderboard.py
@@ -3,31 +3,6 @@ from services.get_encoded_user_scores_from_msg_embeds import (
 )
 from services.try_decode_base64_json import try_decode_base64_json
 
-# async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
-#     leaderboard_message = None
-#     async for message in leaderboard_channel.history():
-#         if message.author == client.user:
-#             leaderboard_message = message
-#             break
-#     if leaderboard_message is None:
-#         raise ValueError("ERROR: no leaderboard message found in leaderboard channel")
-#     user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_message)
-#     return try_decode_base64_json(user_scores_base64)
-
-
-# async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
-#     decoded_user_scores = []
-#     async for message in leaderboard_channel.history(limit=None):
-#         if message.author == client.user:
-#             encoded_user_scores = get_encoded_user_scores_from_msg_embeds(message)
-#             if encoded_user_scores != None:
-#                 decoded_data = try_decode_base64_json(encoded_user_scores)
-#                 if decoded_data != None:
-#                     decoded_user_scores.append(decoded_data)
-#     if decoded_user_scores is None:
-#         raise ValueError("ERROR: no leaderboard message found in leaderboard channel")
-#     return decoded_user_scores
-
 async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
     leaderboard_messages = []
     user_scores_base64_list = []
