a1fe445d6295731b85c88ff617d082a7c9b79feb
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 15:45:01 -0500

    Update upsert_message_for_channel.py

--- features/update_leaderboard/upsert_message_for_channel.py
+++ features/update_leaderboard/upsert_message_for_channel.py
@@ -1,10 +1,15 @@
 async def upsert_message_for_channel(channel, embed, client):
-    the_msg = None
-    async for message in channel.history():
-        if message.author == client.user and message.embeds and message.embeds[0].title == embed.title:
-            the_msg = message
-            break
-    if the_msg is None:
-        the_msg = await channel.send(embed=embed)
-    else:
-        await the_msg.edit(embed=embed)
+    try:
+        the_msg = None
+        async for message in channel.history():
+            if message.author == client.user:
+                if message.embeds and message.embeds[0].title == embed.title:
+                    the_msg = message
+                    break
+        if the_msg is None:
+            the_msg = await channel.send(embed=embed)
+        else:
+            await the_msg.edit(embed=embed)
+    except Exception as e:
+        # Handle any errors that occur during the process
+        raise ValueError(f"Error upserting message for channel: {str(e)}")
