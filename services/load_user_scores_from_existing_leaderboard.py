802af4146bbdf6ecc7ea50059c96b624a1dc1960
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 18:09:55 -0500

    Update load_user_scores_from_existing_leaderboard.py

--- services/load_user_scores_from_existing_leaderboard.py
+++ services/load_user_scores_from_existing_leaderboard.py
@@ -15,16 +15,31 @@ from services.try_decode_base64_json import try_decode_base64_json
 #     return try_decode_base64_json(user_scores_base64)
 
 
+# async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
+#     decoded_user_scores = []
+#     async for message in leaderboard_channel.history(limit=None):
+#         if message.author == client.user:
+#             encoded_user_scores = get_encoded_user_scores_from_msg_embeds(message)
+#             if encoded_user_scores != None:
+#                 decoded_data = try_decode_base64_json(encoded_user_scores)
+#                 if decoded_data != None:
+#                     decoded_user_scores.append(decoded_data)
+#     if decoded_user_scores is None:
+#         raise ValueError("ERROR: no leaderboard message found in leaderboard channel")
+#     return decoded_user_scores
+
 async def load_user_scores_from_existing_leaderboard(leaderboard_channel, client):
-    decoded_user_scores = []
-    async for message in leaderboard_channel.history(limit=None):
+    leaderboard_messages = []
+    user_scores_base64_list = []
+    async for message in leaderboard_channel.history():
         if message.author == client.user:
-            encoded_user_scores = get_encoded_user_scores_from_msg_embeds(message)
-            if encoded_user_scores != None:
-                decoded_data = try_decode_base64_json(encoded_user_scores)
-                if decoded_data != None:
-                    decoded_user_scores.append(decoded_data)
-    if decoded_user_scores is None:
+            leaderboard_messages.append(message)
+    for leaderboard_message in leaderboard_messages:
+        user_scores_base64 = get_encoded_user_scores_from_msg_embeds(leaderboard_message)
+        user_scores_base64_list.append(user_scores_base64)
+    combined_base64 = "".join(user_scores_base64_list)
+    decoded_user_scores = try_decode_base64_json(combined_base64)
+    if not decoded_user_scores:
         raise ValueError("ERROR: no leaderboard message found in leaderboard channel")
     return decoded_user_scores
 
