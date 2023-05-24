cbe8e05362858ec2404c6ee1cd15f79b86522b56
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 15:05:06 -0500

    Update upsert_message_for_channel.py

--- features/update_leaderboard/upsert_message_for_channel.py
+++ features/update_leaderboard/upsert_message_for_channel.py
@@ -2,7 +2,8 @@ async def upsert_message_for_channel(channel, embed, client):
     the_msg = None
     async for message in channel.history():
         if message.author == client.user:
-            the_msg = message
+            if message.title == embed.title:
+                the_msg = message
             break
     if the_msg is None:
         the_msg = await channel.send(embed=embed)
